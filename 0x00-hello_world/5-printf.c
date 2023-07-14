2c84e7ab3a6feb0b4bfbd8929095572ff00fe20d
rasaq-ola <rasaqola196@gmail.com>
2023-07-13 20:09:26 -0700

    m

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  * return: 0 (success)
  */
 int main(void);
 {
-	printf("with proper grammar, but the outcome is a poece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a poece of art,\n");
+return (0);
+}
