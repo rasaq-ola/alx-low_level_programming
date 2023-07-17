5395e6a1ea98022acdafc949ea39ea3d0564ee78
rasaq-ola <rasaqola196@gmail.com>
2023-07-17 12:42:58 -0700

    m

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - Entry point
  * return: 0 (success)
  */
-int main(void);
+int main(void)
 {
-printf("with proper grammar, but the outcome is a poece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
