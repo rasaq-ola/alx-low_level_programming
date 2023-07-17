3fa44b935243a6e4a3d57d519ee1b0cf8a0abae4
rasaq-ola <rasaqola196@gmail.com>
2023-07-17 14:14:08 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 int main(void)
 {
 	write(2,
-	"nd that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
