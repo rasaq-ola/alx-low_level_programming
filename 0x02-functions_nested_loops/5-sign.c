aac02422b1154fca1b9e8d766d5be02cdf427931
rasaq-ola <rasaqola196@gmail.com>
2023-07-18 18:14:42 -0700

    m

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,15 +12,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
-	 else if (n == 0)
-	
-		_putchar(0);
-		return (48);
-	 else if (n < 0)
-	
-		_putchar('-');
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
 		return (-1);
 	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
 }
