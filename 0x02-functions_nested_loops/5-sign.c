8869801dc525434b0ad061d50469ec90e7e4b01e
rasaq-ola <rasaqola196@gmail.com>
2023-07-19 11:12:05 -0700

    m

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,22 +7,16 @@
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
-int print_sign(int n)
-{
-    if (n > 0)
-    {
+
+int print_sign(int n) {
+    if (n > 0) {
         putchar('+');
         return 1;
-    }
-    else if (n == 0)
-    {
+    } else if (n < 0) {
+        putchar('-');
+        return -1;
+    } else {
         putchar('0');
         return 0;
     }
-    else if (n < 0)
-    {
-        putchar('-');
-        return -1;
-    }
-    return 0;
 }
