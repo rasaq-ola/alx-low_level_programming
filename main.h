aadb67bc7533b5c21d4304354c62e4948dcef0ed
rasaq-ola <rasaqola196@gmail.com>
2023-09-30 15:20:24 -0700

    m

---
+++ main.h
@@ -0,0 +1,13 @@
+#define MAIN_H
+#ifndef MAIN_H
+
+void _print_rev_recursion(char *s);
+int _putchar(char c);
+int _strlen_recursion(char *s);
+int is_palindrome(char *s);
+void _puts_recursion(char *s);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int is_prime_number(int n);
+int _sqrt_recursion(int n);
+#endif
