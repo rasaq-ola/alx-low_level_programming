51bd9429b43df79572bcab308f503e09e12fa82d
rasaq-ola <rasaqola196@gmail.com>
2023-10-10 15:30:43 -0700

    0x0A. C - argc, argv

--- main.h
+++ main.h
@@ -1,13 +1,7 @@
-#define MAIN_H
 #ifndef MAIN_H
+#define MAIN_H
 
-void _print_rev_recursion(char *s);
 int _putchar(char c);
-int _strlen_recursion(char *s);
-int is_palindrome(char *s);
-void _puts_recursion(char *s);
-int _pow_recursion(int x, int y);
-int factorial(int n);
-int is_prime_number(int n);
-int _sqrt_recursion(int n);
+int _atoi(char *s);
+
 #endif
