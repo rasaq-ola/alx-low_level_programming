59e7422599d36f599e1ab804a57d82c5c8d77e2f
rasaq-ola <olatech@DESKTOP-2QK9NGF>
2024-05-21 18:05:24 +0100

    search algorithm

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,45 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * binary_search - searches for a value in a sorted array of integers using Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: the index where value is located, or -1 if value is not present or array is NULL
+ */
+int binary_search(int *array, size_t size, int value)
+{
+    size_t left, right, mid;
+    size_t i;
+
+    if (array == NULL)
+        return (-1);
+
+    left = 0;
+    right = size - 1;
+
+    while (left <= right)
+    {
+        printf("Searching in array: ");
+        for (i = left; i <= right; i++)
+        {
+            printf("%d", array[i]);
+            if (i != right)
+                printf(", ");
+        }
+        printf("\n");
+
+        mid = (left + right) / 2;
+
+        if (array[mid] == value)
+            return (mid);
+        else if (array[mid] < value)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,23 @@
+# Search Algorithms
+
+This project implements search algorithms in C.
+
+## Files
+
+- `search_algos.h`: Header file with function prototypes.
+- `0-linear.c`: Implementation of the linear search algorithm.
+- `1-binary.c`: Implementation of the binary search algorithm.
+- `2-O`: Time complexity of linear search.
+- `3-O`: Space complexity of linear search.
+- `4-O`: Time complexity of binary search.
+- `5-O`: Space complexity of binary search.
+- `6-O`: Space complexity of the `allocate_map` function.
+
+## Compilation
+
+To compile the code, use the following commands:
+
+```sh
+gcc -Wall -Wextra -Werror -pedantic -std=gnu89 0-main.c 0-linear.c -o 0-linear
+gcc -Wall -Wextra -Werror -pedantic -std=gnu89 1-main.c 1-binary.c -o 1-binary
+
